8d0f8e58bee32b21af62c1c118888e77026bfe62
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-19 14:55:22 -0400

    Create spring security configuration for login

---
+++ src/main/java/com/login/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.login.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // Add our users for in memory authentication.
+        // withDefaultPasswordEncoder() is deprecated but ok for initial setup.
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("MANAGER"))
+                .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+}
---
+++ src/main/java/com/login/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.login.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+
+}
