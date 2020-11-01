57cebbc14de9300cb8427d4722e3755ed4449ada
Eric Balawejder <eric.balawejder@gmail.com>
2020-11-01 01:10:46 -0400

    Set up JDBC database authentication

--- src/main/java/com/login/springsecurity/demo/config/AppConfig.java
+++ src/main/java/com/login/springsecurity/demo/config/AppConfig.java
@@ -1,23 +1,76 @@
 package com.login.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.login.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig {
 
-    // define a bean for ViewResolver
+    // set up variable to hold the properties
+    @Autowired
+    private Environment environment;
+    private Logger logger = Logger.getLogger(getClass().getName());
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    // define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource() throws PropertyVetoException {
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver class
+        securityDataSource.setDriverClass(environment.getProperty("jdbc.driver"));
+
+        // log this info just to make sure we are reading data from properties file
+        logger.info(">>> jdbc.url=" + environment.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(environment.getProperty("jdbc.url"));
+        securityDataSource.setUser(environment.getProperty("jdbc.user"));
+        securityDataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        // set connection pool props
+        securityDataSource.setInitialPoolSize(
+                getIntProperty("connection.pool.initialPoolSize"));
+
+        securityDataSource.setMinPoolSize(
+                getIntProperty("connection.pool.minPoolSize"));
+
+        securityDataSource.setMaxPoolSize(
+                getIntProperty("connection.pool.maxPoolSize"));
+
+        securityDataSource.setMaxIdleTime(
+                getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propertyName) {
+        String propertyValue = environment.getProperty(propertyName);
+        return Integer.parseInt(propertyValue);
     }
 }
